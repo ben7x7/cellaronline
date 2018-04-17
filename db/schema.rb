c0a6f70002254e6ebb0c81b998411726f6268488
benoit bessiere <benoit.bessiere@gmail.com>
2018-04-17 17:44:34 +0200

    Model Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413180119) do
+ActiveRecord::Schema.define(version: 20180417122004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,6 +42,20 @@ ActiveRecord::Schema.define(version: 20180413180119) do
     t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wines", force: :cascade do |t|
+    t.string "color"
+    t.string "origin"
+    t.string "region"
+    t.string "producer"
+    t.string "appelation"
+    t.integer "millesim"
+    t.integer "price"
+    t.string "sticker"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+User.destroy_all
+
+wines = Category.create!(name: 'Wines')
+spirits = Category.create!(name: 'Spirits')
+beers = Category.create!(name: 'Beers')
+
+
+user1 = User.create!(email: 'brice@yahoo.fr', password: 'password', first_name: 'Brice', last_name: 'DeNice', mobile: '0612345678', address: 'promenade des Anglais 06000 Nice', uid:'', user_type:'')
